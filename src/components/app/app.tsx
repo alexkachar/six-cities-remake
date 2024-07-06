39685bd150190c276e316d461394d5bb93359e1f
Alexander Kachar <alexkachar.github@gmail.com>
2024-07-06 15:04:35 +0300

    Added dev-router and OfferDetails

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,26 @@
-import Main from '../main/main.jsx';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const App = ({ offers, offersCount }) => (
-  <Main offers={offers} offersCount={offersCount} />
-);
+import Main from '../main/main.jsx';
+import OfferDetails from '../offer-details/offer-details.jsx';
+
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { offers, offersCount } = this.props;
+
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main offers={offers} offersCount={offersCount} />} />
+          <Route path="/dev-details" element={<OfferDetails offer={offers[0]} />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
